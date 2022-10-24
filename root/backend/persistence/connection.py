930029d49cb9871d5021945d62441cca064bc5d1
noahmtnr <noah.mautner@outlook.de>
2022-10-25 02:22:28 +0900

    added resume fetch

--- root/backend/persistence/connection.py
+++ root/backend/persistence/connection.py
@@ -5,6 +5,7 @@ import json
 
 config = dotenv_values(".env")
 
+
 class Connection:
 
     def __init__(self) -> None:
@@ -20,21 +21,27 @@ class Connection:
         self.mongodb_client = MongoClient(config["ATLAS_URI"])
         self.database = self.mongodb_client[config["DB_NAME"]]
         self.server_api = ServerApi('1')
-        self.resume_collection = self.database.resumes
-        print("Connected to the MongoDB database!")
-
-    def load_one_json(self, filename):
-        collection = self.resume_collection
-        f = open(filename)
-        my_dict = json.load(f)
-        print(my_dict)
-        result = collection.insert_one(my_dict)
-        print(result)
+        self.resume_collection = self.database[config["RESUME_COLLECTION"]]
+        print("Connected to DB")
 
     def shutdown_connection(self):
         self.mongodb_client.close()
+        print("Connection to DB closed")
+
+    def fetch_all_resumes(self):
+        return list(self.resume_collection.find({}))
+
+    def insert_resume_from_file(self, filename: str):
+        f = open(filename)
+        my_dict = json.load(f)
+
+        self.insert_resume(my_dict)
+
+    def insert_one_resume(self, resume_json: dict):
+        result = self.resume_collection.insert_one(resume_json)
 
 
 db = Connection()
-db.load_one_json("DS2.json")
+resumes = db.fetch_all_resumes()
+print(resumes)
 db.shutdown_connection()
