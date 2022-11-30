302b5525996334265e591d951cd5b79e2cd4fc49
William Damgaard Lauritzen <201808163@post.au.dk>
2022-11-30 13:34:14 +0900

    Basic refactoring

--- root/frontend/src/Output/Output.tsx
+++ root/frontend/src/Output/Output.tsx
@@ -8,7 +8,7 @@ import { MoonLoader } from "react-spinners";
 type OutputLoadingState = "loading" | "done";
 
 function Output() {
-  const [cv, setUnlebeledCV] = useState(new CV()); // Initialize with an empty CV until an unlabeled one is fetched from DB
+  const [cvUnlabeled, setUnlabeledCV] = useState(new CV()); // Initialize with an empty CV until an unlabeled one is fetched from DB
 
   const [cvAnalyzed, setAnalyzedCV] = useState(new CV()); // Initialize with an empty CV until an analyzed one is fetched from DB
 
@@ -34,7 +34,7 @@ function Output() {
   useEffect(() => {
     getUnlabeled().then((unlabeledCV: CV) => {
       console.log(unlabeledCV);
-      setUnlebeledCV(unlabeledCV);
+      setUnlabeledCV(unlabeledCV);
       getAnalysis(JSON.stringify(unlabeledCV)).then((analyzedCV: CV) => {
         console.log(analyzedCV);
         setOutputLoadingState("done");
@@ -44,21 +44,21 @@ function Output() {
   }, []);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <div>
       <div className="split left">
         <label>
           <h1>Previous CV:</h1>
         </label>
         <label>
-          <h2>{cv.title}</h2>
+          <h2>{cvUnlabeled.title}</h2>
         </label>
         <div className="section">
           <h2 className="item">Summary</h2>
           <div className="rating">
-            <p className="item">{cv.summary.value}</p>
+            <p className="item">{cvUnlabeled.summary.value}</p>
           </div>
         </div>
-        {mapFields_P()}
+        {mapFields(cvUnlabeled)}
       </div>
       <div className="bar"></div>
       <div className="split right">
@@ -76,7 +76,7 @@ function Output() {
           </div>
         ) : (
           <>
-            <button onSubmit={(e) => handleSubmit(e)}>Download</button>
+            <button onClick={() => download()}>Download</button>
             <label>
               <h1>Your winning CV:</h1>
             </label>
@@ -89,30 +89,14 @@ function Output() {
                 <p className="item">{cvAnalyzed.summary.value}</p>
               </div>
             </div>
-            {mapFields()}
+            {mapFields(cvAnalyzed)}
           </>
         )}
       </div>
-    </form>
+    </div>
   );
 
-  function mapFields() {
-    return (
-      <div>
-        {Object.keys(cvAnalyzed).map((field) => {
-          if (field === "title" || field === "summary" || field === "_id") {
-            return;
-          }
-          if (field === "contactInfo") {
-            return; //mapField(field)
-          }
-          return mapNestedField(field);
-        })}
-      </div>
-    );
-  }
-
-  function mapFields_P() {
+  function mapFields(cv: CV) {
     return (
       <div>
         {Object.keys(cv).map((field) => {
@@ -122,88 +106,13 @@ function Output() {
           if (field === "contactInfo") {
             return; //mapField(field)
           }
-          return mapNestedField_P(field);
+          return mapNestedField(cv, field);
         })}
       </div>
     );
   }
 
-  function mapField(field: string) {
-    // Verify field
-    switch (field) {
-      case Field.ContactInfo:
-        break;
-      default:
-        console.log("Unrecognized field: " + field);
-        return <p>ERROR: UNRECOGNIZED FIELD - {field}</p>;
-    }
-
-    return (
-      <div className="section">
-        <h2 className="item">{field}</h2>
-        <div className="item">
-          {Object.keys(cv[field]).map((key, index) => {
-            return (
-              <p>
-                <b>{key}: </b> {Object.values(cv[field])[index]}
-              </p>
-            );
-          })}
-        </div>
-      </div>
-    );
-  }
-
-  function mapNestedField(field: string) {
-    // Verify field
-    switch (field) {
-      case Field.Interests:
-      case Field.Patents:
-      case Field.Extracurriculars:
-      case Field.Accomplishments:
-      case Field.Projects:
-      case Field.SoftSkills:
-      case Field.HardSkills:
-      case Field.Languages:
-      case Field.Experience:
-      case Field.Certifications:
-      case Field.Education:
-        break;
-      default:
-        console.log("Unrecognized field: " + field);
-        return <p>ERROR: UNRECOGNIZED FIELD - {field}</p>;
-    }
-
-    // We do not want to show empty lists
-    if (cvAnalyzed[field].value.length === 0) {
-      return;
-    }
-
-    // Map field values
-    return (
-      <div className="section">
-        <h2 className="item">{field}</h2>
-        {/* {label(field)} */}
-        {cvAnalyzed[field].value.map((listItem, index) => {
-          return (
-            <div className="rating" key={index}>
-              <div className={index % 2 === 0 ? "item light" : "item dark"}>
-                {Object.keys(listItem.value).map((key, index) => {
-                  return (
-                    <p>
-                      <b>{key}: </b> {Object.values(listItem.value)[index]}
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
-
-  function mapNestedField_P(field: string) {
+  function mapNestedField(cv: CV, field: string) {
     // Verify field
     switch (field) {
       case Field.Interests:
@@ -251,83 +160,8 @@ function Output() {
     );
   }
 
-  function label(field: string, index?: number) {
-    return (
-      <input
-        type="number"
-        min="1"
-        max="10"
-        name="rating"
-        onChange={(e) => {
-          let label = Number(e.target.value);
-          if (index === undefined) {
-            labelItem(field, label);
-          } else {
-            labelListItem(field, label, index);
-          }
-        }}
-      />
-    );
-  }
-
-  function labelItem(field: string, label: number) {
-    switch (field) {
-      case "summary":
-      case Field.Interests:
-      case Field.Patents:
-      case Field.Extracurriculars:
-      case Field.Accomplishments:
-      case Field.Projects:
-      case Field.SoftSkills:
-      case Field.HardSkills:
-      case Field.Languages:
-      case Field.Experience:
-      case Field.Certifications:
-      case Field.Education:
-        let data = cv[field];
-        let value = data.value;
-        data.label = label;
-        setUnlebeledCV((prevState) => ({
-          ...prevState,
-          [field]: { label: label, value: value },
-        }));
-        break;
-      default:
-        console.log("Unknown field: " + field);
-    }
-  }
-
-  function labelListItem(field: string, label: number, index: number) {
-    switch (field) {
-      case Field.Interests:
-      case Field.Patents:
-      case Field.Extracurriculars:
-      case Field.Accomplishments:
-      case Field.Projects:
-      case Field.SoftSkills:
-      case Field.HardSkills:
-      case Field.Languages:
-      case Field.Experience:
-      case Field.Certifications:
-      case Field.Education:
-        let value = cv[field].value;
-        let listLabel = cv[field].label;
-        value[index].label = label;
-        setUnlebeledCV((prevState) => ({
-          ...prevState,
-          [field]: { label: listLabel, value: value },
-        }));
-        break;
-      default:
-        console.log("Unknown field: " + field);
-    }
-  }
-
-  function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    let json = JSON.stringify(cv);
-    console.log(json);
-    //uploadLabeled(json).then(e => console.log(e))
+  function download() {
+    console.log("Do download stuff.")
   }
 }
 
