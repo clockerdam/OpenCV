4eb19ce4fb8a2c1f73218dbbb263350fbcea08fe
Lucas Lo Scalzo <lucas.loscalzo9@gmail.com>
2022-12-02 16:27:57 +0900

    job title input added

--- root/frontend/src/Output/Tabs/Job.tsx
+++ root/frontend/src/Output/Tabs/Job.tsx
@@ -11,7 +11,13 @@ function Job() {
             <button onSubmit={(e) => handleSubmit(e)}>Submit</button>
             <label>
                 <p>Your job description that will tailor your resume:</p>
-                <input 
+                <input
+                    name="job_title"
+                    placeholder="Input the job title"
+                    value={job.job_title}
+                    onChange={(e) => setJob(prevState => ({ ...prevState, job_title: e.target.value}))}
+                />
+                <input className="textArea"
                     name="job_description"
                     placeholder="Input the job description"
                     value={job.job_description}
