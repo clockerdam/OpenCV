d7046b4b02b8d908bbcfc7b79db7e28993174b3b
Øyvind Monsen <yvind.monsen@gmail.com>
2022-12-05 15:25:46 +0900

    fix: remove keywords from model output json

--- root/backend/prediction/prediction.py
+++ root/backend/prediction/prediction.py
@@ -38,6 +38,7 @@ def improve_cv(payload: dict) -> dict:
     # Filter out unwanted fields
 
     cut = cut[resume_df.columns]
+    cut = cut.drop("keywords", axis=1)
 
     finished_resume = get_resume_dict_from_dataframe(cut, md)
 
