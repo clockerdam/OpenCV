ab67d2a58e1e817aea38dd8dd13ab021a31f2ae9
Øyvind <yvind.monsen@gmail.com>
2022-12-07 19:42:17 +0900

    fix: showing pdf after cutting (#28)
    * fix: showing pdf after cutting
    
    * fix: correct linting errors
    
    * fml
    
    * remove unused content

--- root/frontend/src/InputView/Output/Tabs/Pdf.tsx
+++ root/frontend/src/InputView/Output/Tabs/Pdf.tsx
@@ -1,20 +1,30 @@
+import { useContext, } from 'react';
+import cvContext from '../../../cvContext';
 import './tabs.css'
 
 //Option: https://www.pdftron.com/blog/react/how-to-build-a-react-pdf-viewer/#step-5-implementing-pdfjs
 
 //http://africau.edu/images/default/sample.pdf
 
-function Pdf(){
+function Pdf() {
+  const { pdfData } = useContext(cvContext)
+
+
   return (
     <div className="FirstTab">
       {/* <p>Pdf Tab!! Hurray!!</p> */}
       {/* First tab content will go here */}
       {/* <embed src="CV.pdf" width="100%" height="600px" /> */}
-      <object data="http://localhost:8080/pdf" width="500px" height="1000px">
-        {/* <p>Alternative text - include a link <a href="http://africau.edu/images/default/sample.pdf">to the PDF!</a></p> */}
-      </object>
+      {pdfData !== "" &&
+        <>
+          <object style={{ width: "100%" }} data={pdfData} type="application/pdf" width="500px" height="1000px">
+            The object tag is not supported in your browser
+          </object>
+        </>
+
+      }
     </div>
   );
 };
-export {Pdf};
+export { Pdf };
 
