23f0ed917bfca24b002c720d831aa01ef7a316d1
LucasMaertens <lucas.maertens@student.kuleuven.be>
2022-12-08 22:00:50 +0900

    fixed import error

--- root/frontend/src/HomePage/routes/Inputpage.tsx
+++ root/frontend/src/HomePage/routes/Inputpage.tsx
@@ -1,14 +1,14 @@
-import Navbar from '../components/Navbar'
+
 import React  from 'react';
 
-import Footer from '../components/Footer';
+
 import { Form } from '../../InputView/Form/Form';
 function Home(){
     return (
         <>
-        <Navbar />
+        
         <Form />
-        <Footer/>
+        
         </>
     );
 }
